b7a6c99ade7142c54b2aa7cf03a9f242d4379ad0
fatkwong14 <fatkwong14@gmail.com>
2017-01-31 23:18:50 +0800

    other me like eason

--- SalesTool/src/com/main/HelloWorld.java
+++ SalesTool/src/com/main/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.main;
 
 public class HelloWorld {
-
+	private String defaultSong = "Eason Song is stylish";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("testomg");
+		System.out.print("testomg "+dataPatch());
+	}
+	
+	public static String dataPatch(){
+		return "data patcing ...";
 	}
 
 }
